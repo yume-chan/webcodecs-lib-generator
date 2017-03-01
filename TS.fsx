8bc5d6d4929a0ef6a6a3d650dcc9a0cd9d3b50bd
zhengbli <zhengbli@microsoft.com>
2017-02-28 18:59:41 -0800

    Make events use 'this' as eventTarget when possible

--- TS.fsx
+++ TS.fsx
@@ -672,6 +672,10 @@ module Data =
     let extendConflictsBaseTypes =
         extendConflicts |> List.map (fun ec -> (ec.BaseType, ec)) |> Map.ofList
 
+    // these types are only mixins to define shared events, they don't extend the EventTarget interface, therefore no need to
+    // emit "addEventListener" for them
+    let nonEventTargetTypes = ["GlobalEventHandlers"; "AbstractWorker"; "XMLHttpRequestEventTarget"; "MSBaseReader"]
+
 module Emit =
     open Data
     open Types
@@ -903,6 +907,11 @@ module Emit =
              | Some pollutor -> "this: " + pollutor.Name + ", "
              | _ -> ""
 
+    let ComputerEventTypeParameter eventType flavor (prefix: string) =
+        if List.contains eventType nonEventTargetTypes then ""
+        else 
+            if prefix.StartsWith("declare ") then "<" + GetGlobalPollutorName flavor + ">" else "<this>"
+
     let EmitProperties flavor prefix (emitScope: EmitScope) (i: Browser.Interface) (conflictedMembers: Set<string>) =
         let emitPropertyFromJson (p: InputJsonType.Root) =
             let readOnlyModifier =
@@ -939,7 +948,11 @@ module Emit =
                                     getEventTypeInInterface p.EventHandler.Value i
                                 else
                                     "Event"
-                            String.Format("({0}ev: {1}) => any", EmitEventHandlerThis flavor prefix i, eType)
+                            String.Format(
+                                "({0}ev: {1}{2}) => any",
+                                EmitEventHandlerThis flavor prefix i,
+                                eType,
+                                ComputerEventTypeParameter i.Name flavor prefix)
                         | _ -> DomTypeToTsType p.Type
                     let pTypeAndNull = if p.Nullable.IsSome then makeNullable pType else pType
                     let readOnlyModifier = if p.ReadOnly.IsSome && prefix = "" then "readonly " else ""
@@ -1075,12 +1088,8 @@ module Emit =
                 "%saddEventListener<K extends keyof %sEventMap>(type: K, listener: (this: %s, ev: %sEventMap[K]) => any, useCapture?: boolean): void;"
                 prefix iParent.Name i.Name iParent.Name
 
-        // these types are only mixins to define shared events, they don't extend the EventTarget interface, therefore no need to
-        // emit "addEventListener" for them
-        let specialTypes = ["GlobalEventHandlers"; "AbstractWorker"; "XMLHttpRequestEventTarget"; "MSBaseReader"]
-
         let shouldEmitStringEventHandler =
-            if List.contains i.Name specialTypes then
+            if List.contains i.Name nonEventTargetTypes then
                 false
             elif iNameToEhList.ContainsKey i.Name  && not iNameToEhList.[i.Name].IsEmpty then
                 emitEventHandler fPrefix i
