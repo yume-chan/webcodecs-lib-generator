fd21fc56a3bb1496eebe70574d28054ad3631375
Orta <git@orta.io>
2021-08-03 19:11:26 +0100

    Adds a comment about why it's not null

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -17230,6 +17230,7 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
     readonly pageXOffset: number;
     /** @deprecated This is a legacy alias of `scrollY`. */
     readonly pageYOffset: number;
+    /** Refers to either the parent WindowProxy, or itself. If you create a child context, then remove the parent you could get null though. */
     readonly parent: WindowProxy;
     /**
      * Returns true if the personal bar is visible; otherwise, returns false.
@@ -18294,6 +18295,7 @@ declare var outerWidth: number;
 declare var pageXOffset: number;
 /** @deprecated This is a legacy alias of `scrollY`. */
 declare var pageYOffset: number;
+/** Refers to either the parent WindowProxy, or itself. If you create a child context, then remove the parent you could get null though. */
 declare var parent: WindowProxy;
 /**
  * Returns true if the personal bar is visible; otherwise, returns false.
