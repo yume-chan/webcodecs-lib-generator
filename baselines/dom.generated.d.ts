58c51a6c83809d68fdd26299a8c124aaa139edb0
Orta <git@orta.io>
2021-08-04 13:10:41 +0100

    Expand copy

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -17230,7 +17230,7 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
     readonly pageXOffset: number;
     /** @deprecated This is a legacy alias of `scrollY`. */
     readonly pageYOffset: number;
-    /** Refers to either the parent WindowProxy, or itself. If you create a child context, then remove the parent you could get null though. */
+    /** Refers to either the parent WindowProxy, or itself. It can rarely be null e.g. for contentWindow of an iframe that is already removed from the parent. */
     readonly parent: WindowProxy;
     /**
      * Returns true if the personal bar is visible; otherwise, returns false.
@@ -18295,7 +18295,7 @@ declare var outerWidth: number;
 declare var pageXOffset: number;
 /** @deprecated This is a legacy alias of `scrollY`. */
 declare var pageYOffset: number;
-/** Refers to either the parent WindowProxy, or itself. If you create a child context, then remove the parent you could get null though. */
+/** Refers to either the parent WindowProxy, or itself. It can rarely be null e.g. for contentWindow of an iframe that is already removed from the parent. */
 declare var parent: WindowProxy;
 /**
  * Returns true if the personal bar is visible; otherwise, returns false.
